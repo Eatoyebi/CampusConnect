0ef8477ff1d44c4da9c50bacbd88712d6f6aa26b
Elizabeth Atoyebi <144946811+Eatoyebi@users.noreply.github.com>
2025-10-18 18:26:15 -0400

    feat(navbar): implement UC-themed navigation bar with red pill button… (#53)
    * feat(navbar): implement UC-themed navigation bar with red pill buttons and clean layout
    
    * made font bigger

--- frontend/campus-connect/src/app/app.routes.ts
+++ frontend/campus-connect/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'chat',
+    loadComponent: () =>
+      import('./pages/chat/chat').then(m => m.Chat)
+  },
+  {
+    path: 'announcements',
+    loadComponent: () =>
+      import('./pages/announcements/announcements').then(m => m.Announcements)
+  },
+  {
+    path: 'maintenance',
+    loadComponent: () =>
+      import('./pages/maintenance/maintenance').then(m => m.Maintenance)
+  },
+  { path: '**', redirectTo: '' }
+];
