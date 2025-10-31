4084b28dccaacc4e8274803e6ed43fff62e3814d
David Rome <romedt@mail.uc.edu>
2025-10-30 22:41:50 -0400

    create maitenance form

--- frontend/campus-connect/src/app/pages/maintenance/maintenance.ts
+++ frontend/campus-connect/src/app/pages/maintenance/maintenance.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-maintenance',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './maintenance.html',
   styleUrl: './maintenance.css'
 })
 export class Maintenance {
 
+    newRequest = {
+    name: '',
+    mNumber: '',
+    location: '',
+    description: ''
+  }; //object to hold new request data
+
+  requests: any[] = []; //array of requests
+
+onSubmit() {
+  //Logic to submit maintenance request
+    if (this.newRequest.name && this.newRequest.mNumber && this.newRequest.location && this.newRequest.description) { //are all fields filled
+      this.requests.push({ ...this.newRequest }); //add new request to requests array
+      this.newRequest = { name: '', mNumber: '', location: '', description: '' }; //reset form
+      alert('Maintenance request submitted successfully!');
+  } else {
+      alert('Please fill out all fields before pressing submit.');
+  }
+  }
 }
