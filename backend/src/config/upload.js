c350863d16fa92ec3e97fe2ce58d0c0f36fc46ea
David Rome <romedt@mail.uc.edu>
2026-02-01 18:39:12 -0500

    add multer file limit

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import path from 'path';
 
+multer({ limits: { fileSize: 5 * 1024 * 1024 } }) // 5MB file size limit
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
