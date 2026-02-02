5e4046d53cc6e96b68620032f69c388d8ad096e6
David Rome <romedt@mail.uc.edu>
2026-02-01 19:00:40 -0500

    update file names

--- backend/src/routes/maintenanceTicketRoutes.js
+++ backend/src/routes/maintenanceTicketRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { createMaintenanceTicket, getallMaintenanceTickets, updateTicketStatus } from '../controllers/maintenanceController.js';
+import { createMaintenanceTicket, getallMaintenanceTickets, updateTicketStatus } from '../controllers/maintenanceTicketController.js';
 
 const router = express.Router();
 
