c2a2dffc2aa0e4493f5189ca95eb364473a5cd75
David Rome <romedt@mail.uc.edu>
2026-04-13 16:42:41 -0400

    add filter functionality for maintenance view

--- frontend/campus-connect/src/app/pages/maintenance/maintenance-ticket-list/maintenance-ticket-list.ts
+++ frontend/campus-connect/src/app/pages/maintenance/maintenance-ticket-list/maintenance-ticket-list.ts
@@ -9,5 +9,39 @@ import { Ticket } from "../../..//models/ticket.model";
   styleUrl: './maintenance-ticket-list.css',
 })
 export class MaintenanceTicketList {
+tickets: Ticket[] = [];
+filteredTickets: Ticket[] = [];
 
+searchText = '';
+statusFilter = '';
+
+constructor(private ticketService: TicketService) {}
+
+ngOnInit() {
+ this.loadTickets();
+}
+
+loadTickets(){
+  this.ticketService.getMyTickets().subscribe(data =>{
+    this.tickets = data;
+    this.applyFilters();
+  })
+}
+
+applyFilters() {
+  this.filteredTickets = this.tickets.filter(t => {
+
+    const matchSearch = !this.searchText || t.description.toLowerCase().includes(this.searchText.toLowerCase());
+
+    const matchStatus = !this.statusFilter || t.status === this.statusFilter
+
+
+    return matchSearch && matchStatus;
+  });
+}
+
+getStatusCount(status?: string): number {
+  if(!status) return this.tickets.length;
+  return this.tickets.filter(t => t.status === status).length;
+}
 }
